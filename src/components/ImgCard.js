fa63d40c5a836ccb70d71d174c18c8628fb2a2d2
hayderjebur <49797771+hayderjebur@users.noreply.github.com>
2022-07-10 15:06:51 -0400

    Change all classes of css to material-ui classes

--- src/components/ImgCard.js
+++ src/components/ImgCard.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import { Grid, Card, CardMedia, Typography, Box } from '@mui/material';
+import {
+  Grid,
+  Card,
+  CardMedia,
+  Typography,
+  Box,
+  useTheme,
+} from '@mui/material';
+import { styles } from '../styles';
 
 const ImgCard = ({ src }) => {
+  const sx = styles(useTheme());
+
   return (
-    <Grid item sm={6} md={4} lg={3}>
-      <Box className='main'>
-        <Card sx={{ maxWidth: 345 }}>
-          <CardMedia
-            component='img'
-            height='300'
-            image={src}
-            alt='dog'
-            className='image'
-          />
+    <Grid item xs={12} sm={6} md={4} lg={3}>
+      <Box sx={sx.main}>
+        <Card>
+          <CardMedia component='img' height='300' image={src} alt='dog' />
         </Card>
 
-        <Box className='overlay'>
-          <Typography className='text'>Hello dog</Typography>
+        <Box sx={sx.overlay}>
+          <Typography sx={sx.text}>Hello dog</Typography>
         </Box>
       </Box>
     </Grid>
---
+++ src/styles.js
@@ -0,0 +1,38 @@
+export const styles = (theme) => {
+  return {
+    main: {
+      position: 'relative',
+      cursor: 'pointer',
+      '&:hover .css-1o8sc7t': {
+        opacity: 1,
+      },
+      '&:hover .css-1d4pmoe-MuiTypography-root': {
+        transition: 'all 0.3s ease',
+        transform: 'scale(1.8)',
+      },
+    },
+
+    overlay: {
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      height: '100%',
+      width: '100%',
+      opacity: 0,
+      transition: '0.5s ease',
+      backgroundColor: '#fff',
+    },
+    text: {
+      color: '#000',
+      fontSize: '20px',
+      position: 'absolute',
+      textAlign: 'center',
+      top: '50%',
+      left: '35%',
+      transition: 'visibility 0.14s ease-in, opacity 0.14s ease-in',
+      transform: '0.24s ease',
+    },
+  };
+};
